153d507b9848324fce9ccd67c60e910313a78f38
Rohan Verma <hello@rohanverma.net>
2020-05-25 21:20:35 +0530

    feat: refactor to tweet sentences

--- bot.py
+++ bot.py
@@ -24,7 +24,40 @@ def create_api():
     logging.info("API created")
     return api
 
-   
+ # -*- coding: utf-8 -*-
+alphabets= "([A-Za-z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov)"
+
+def split_into_sentences(text):
+    text = " " + text + "  "
+    text = text.replace("\n"," ")
+    text = re.sub(prefixes,"\\1<prd>",text)
+    text = re.sub(websites,"<prd>\\1",text)
+    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
+    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
+    if "”" in text: text = text.replace(".”","”.")
+    if "\"" in text: text = text.replace(".\"","\".")
+    if "!" in text: text = text.replace("!\"","\"!")
+    if "?" in text: text = text.replace("?\"","\"?")
+    text = text.replace(".",".<stop>")
+    text = text.replace("?","?<stop>")
+    text = text.replace("!","!<stop>")
+    text = text.replace("<prd>",".")
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences  
+
 api = create_api()
 
 # read all lines
@@ -48,40 +81,55 @@ while True:
     line = lines[line_to_tweet]  
     if line == "\n":
         line_to_tweet+=1
-    elif re.match("^\d*\.\d*\.\d*", line):
-        line_to_tweet+=1
-        pending_tweet = lines[line_to_tweet]
-        line_to_tweet+=1
     else:
         break
 
-pending_tweet += lines[line_to_tweet]
-# create tweet
-if len(pending_tweet) > MAX_CHAR_FOR_TWEET:
-    new_line_char = min(line_char+MAX_CHAR_FOR_TWEET, len(pending_tweet)) 
+# if it matches with 1.1.1 format, skip line, add name, save to file
+if re.match("^\d*\.\d*\.\d*", line):
+    line_to_tweet+=1
+    pending_tweet = lines[line_to_tweet]
+    line_to_tweet+=1
+    of = open('last_line.txt', "w")
+    of.writelines([str(line_to_tweet), '\n', str(line_char)])
+    of.close()
+        
+# check if it is a sentence
+sentences = split_into_sentences(lines[line_to_tweet][line_char:])
 
-    new_pending_tweet = pending_tweet[line_char:new_line_char]
+if len(sentences) > 0:
+    first_sentence = sentences[0]
     
-    if new_line_char == len(pending_tweet):
+    if len(first_sentence) != len(lines[line_to_tweet]):
+        line_char = lines[line_to_tweet].find(first_sentence) + len(first_sentence)
+
+    pending_tweet += first_sentence
+
+    if len(sentences) == 1:
         line_char = 0
         line_to_tweet += 1
-    else:
-        line_char = new_line_char
-
-    pending_tweet = new_pending_tweet
 else:
-    line_to_tweet +=1
-    
-logging.info("sending tweet: "+pending_tweet)
+    pending_tweet += lines[line_to_tweet]
+    line_to_tweet+=1
+    line_char = 0
 
-try:
-    api.update_status(status=pending_tweet)
-except tweepy.TweepError as error:
-    if error.api_code == 187:
-        # Do something special
-        api.update_status(status=pending_tweet+".")
-    else:
-        raise error
+# create tweet  
+logging.info("sending tweet: ===\n"+pending_tweet+"\n===")
+
+out_tweet = ""
+while True: 
+    out_tweet = pending_tweet[:240]
+    try:
+        api.update_status(status=out_tweet)
+    except tweepy.TweepError as error:
+        if error.api_code == 187:
+            # Do something special
+            api.update_status(status=out_tweet+".")
+        else:
+            raise error
+    
+    pending_tweet = pending_tweet[240:]
+    if len(pending_tweet) == 0:
+        break
 
 of = open('last_line.txt', "w")
 of.writelines([str(line_to_tweet), '\n', str(line_char)])
